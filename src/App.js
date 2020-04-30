630368756a8a9f87972e1ab712bd189ff7df75a0
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-04-29 21:43:26 -0300

    feat: add react-router-dom
    Adds react-router-dom lib to handle routing. Also, adds a new page
    just to display content.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 import saleCartReducer from './reducers/saleCart';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import ProductList from './pages/ProductList';
 
 let store = createStore(combineReducers({saleCart: saleCartReducer}));
 const App = () => {
   return (
     <Provider store={store}>
-      <div className='App'>
-        Content here
-      </div>
+      <Router>
+        <div className='App'>
+        <Switch>
+          <Route path='/'>
+            <ProductList />
+          </Route>
+        </Switch>
+        </div>
+      </Router>
     </Provider>
   );
 }
