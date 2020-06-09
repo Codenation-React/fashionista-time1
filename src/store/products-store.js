12abb10127d2c26af30ccfd368d8b656b8beed9b
Gyubido <gubarcaro@gmail.com>
2020-06-09 19:56:28 -0300

    feat: store

---
+++ src/store/products-store.js
@@ -0,0 +1,10 @@
+import { initStore } from './store';
+
+const configureStore = () => {
+    const actions = {
+
+    };
+    initStore(actions, { products: {} });
+};
+
+export default configureStore;
