a0b439f441b6f09a48d0035412a9eeb6fed52b1b
Eric Madureira <ericmadureira.uesc@gmail.com>
2020-06-30 00:55:51 -0300

    feat: product list
    Improves the product list page to fetch data from the api, memoize
    the values and render ProductItems based on that.

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 
-const ProductList = () => (
-  <div>
-    Content here!
-  </div>
-);
+import ProductItem from '../components/ProductItem';
 
-export default ProductList;
+const ListContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const ProductList = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 80%;
+`;
+
+const Container = () => {
+  const [catalog, setCatalog] = useState([]);
+
+  const fetchCatalog = useCallback(() => {
+    const catalogUrl = 'https://5e9935925eabe7001681c856.mockapi.io/api/v1/catalog';
+    axios.get(catalogUrl)
+      .then(response => setCatalog(response.data))
+      .catch(error => {
+        console.log(`There was an error during the fetch: ${error}`);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchCatalog();
+  }, [fetchCatalog]);
+
+  const products = useMemo(() => (
+    catalog
+      .map(product => (
+        <ProductItem product={product} />
+      ))
+  ), [catalog]);
+
+  return (
+    <ListContainer>
+      <ProductList>
+        {products}
+      </ProductList>
+    </ListContainer>
+  );
+};
+
+export default Container;
